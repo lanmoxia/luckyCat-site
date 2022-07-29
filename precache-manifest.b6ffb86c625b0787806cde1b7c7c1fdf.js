3bf10fef0a2824d365dda39f2a0587f750cca1a5
lanmoxia <506076395@qq.com>
2022-07-29 19:47:49 +0800

    deploy

---
+++ precache-manifest.b6ffb86c625b0787806cde1b7c7c1fdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8f0d48ce5cf5153eb5c",
+    "url": "/luckyCat-site/css/app.1f8b2bab.css"
+  },
+  {
+    "revision": "a78032239c95a4cd13dead8b93b283fc",
+    "url": "/luckyCat-site/img/qrcode.png"
+  },
+  {
+    "revision": "38ce980c78b12cf67004efc62509565b",
+    "url": "/luckyCat-site/index.html"
+  },
+  {
+    "revision": "a8f0d48ce5cf5153eb5c",
+    "url": "/luckyCat-site/js/app.7b86dd73.js"
+  },
+  {
+    "revision": "0d1e86321dce3a2f4131",
+    "url": "/luckyCat-site/js/chunk-vendors.d9228c8c.js"
+  },
+  {
+    "revision": "576b0ed8d0e12a91eb18aef9d74a15c2",
+    "url": "/luckyCat-site/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/luckyCat-site/robots.txt"
+  }
+]);
